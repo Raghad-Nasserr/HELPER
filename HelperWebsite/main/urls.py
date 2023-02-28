0436a194e39f69e5a21ff282212730e052b60f4a
raghad nasser <Ra9g77@gmail.com>
2023-02-28 23:33:12 +0300

    I applied some changes to the views.py file

--- HelperWebsite/main/urls.py
+++ HelperWebsite/main/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path("Delete/help/request/<request_id>/", views.delete_help_request, name="delete_help_request"),
     path("Help/details/<request_id>/", views.help_details, name="help_details"),
     path("Join/us/", views.add_new_helper, name="add_new_helper"),
-    path("Apply/<apply_id>/", views.apply, name="apply_page"),
     path("Reply/<request_id>/", views.add_reply, name="add_reply"),
     path("helper/profile/<user_id>/", views.helper_profile, name="helper_profile"),
     path("client/profile/", views.client_profile, name="client_profile"),
--- HelperWebsite/main/views.py
+++ HelperWebsite/main/views.py
@@ -4,12 +4,13 @@ from .models import Contact,HelpRequest,Helper,Reply
 from django.contrib.auth.models import User
 # Create your views here.
 
-#This function for home page
+
 def home(request : HttpRequest):
+   '''This function for home page'''
    return render(request,'main/index.html') 
 
-#This function for Contact form
 def contact_form(request : HttpRequest):
+   '''This function for Contact form'''
    if request.method == "POST":
          add_contact =Contact(name= request.POST["name"], email = request.POST["email"], subject = request.POST["subject"], message = request.POST["message"])
          add_contact.save()
@@ -18,72 +19,75 @@ def contact_form(request : HttpRequest):
    return render(request,'main/contact.html')
 
 
-#This function will work after the user submits the contact form 
 def contact_received(request : HttpRequest,contact_id):
+    '''This function will work after the user submits the contact form '''
     contact_name=Contact.objects.get(id=contact_id)
     return render(request,'main/contact_received.html',{"contact_name":contact_name})
 
 
-#This function for about us page
 def about_us(request : HttpRequest):
+   '''This function for about us page'''
    return render(request,'main/about.html') 
 
 
-#This function for travel management helper page
 def travel_management_helper(request : HttpRequest):
+   '''This function for travel management helper page'''
    return render(request,'main/travel_management_helper.html') 
 
 
-#This function for event management helper page
 def event_management_helper(request : HttpRequest):
+   '''This function for event management helper page'''
    return render(request,'main/event_management_helper.html') 
 
 
 
-
-#This function is for requests for help
-
 def requests_for_help(request : HttpRequest):
+    '''This function is for requests for help'''
     latest_help_requests = HelpRequest.objects.all()
     context = {"latest_help_requests" : latest_help_requests}
     return render(request,'main/requests_for_help.html',context)
 
 
-#This function is for adding help request 
+
 
 def add_help_request(request : HttpRequest):
+   '''This function is for adding help request '''
    if request.method == "POST":
         #to add a new entry
         new_request = HelpRequest(user=request.user, help_description = request.POST["help_description"], phone_number = request.POST["phone_number"])
         new_request.save()
+        return redirect("main:client_profile")
 
    return render(request,'main/add_help_request.html') 
 
 
-#This function is for updating help request 
+
 
 def update_help_request(request : HttpRequest,request_id):
+   '''This function is for updating help request '''
    update = HelpRequest.objects.get(id=request_id)
    if request.method == "POST":
          update.phone_number = request.POST["phone_number"]
          update.help_description = request.POST["help_description"]
          update.save()
-         return redirect("main:requests_for_help")
+         return redirect("main:client_profile")
    return render(request,'main/update_help_request.html',{'update':update}) 
 
 
-#This function is for deleting help request 
+
 
 def delete_help_request(request : HttpRequest,request_id):
+    '''This function is for deleting help request '''
     delete_request = HelpRequest.objects.get(id=request_id)
     delete_request.delete()
-    return redirect("main:requests_for_help")
+    return redirect("main:client_profile")
 
 
 
 
-#This function for adding new helper
+
 def add_new_helper(request : HttpRequest):
+  '''This function for adding new helper '''
   if request.method == "POST":
          new_user = User.objects.create_user(username=request.POST["username"], email=request.POST["email"], password=request.POST["password"], first_name = request.POST["first_name"], last_name = request.POST["last_name"])
          new_user.save()
@@ -94,18 +98,11 @@ def add_new_helper(request : HttpRequest):
   return render(request,'main/add_new_helper.html')
 
 
-#This function will work after the new helper submits the form 
-def apply(request : HttpRequest,apply_id):
-    helper=Helper.objects.get(id=apply_id)
-    return render(request,'main/apply.html',{"helper":helper})
 
 
-
-
-#This function to let the helper reply to help request 
 
 def add_reply(request : HttpRequest,request_id):
-
+    '''This function to let the helper reply to help request '''
     if request.method == "POST":
         helprequest = HelpRequest.objects.get(id=request_id)
         new_reply = Reply(user=request.user, helprequest=helprequest, content = request.POST["content"], created_at = request.POST["created_at"])
@@ -115,26 +112,29 @@ def add_reply(request : HttpRequest,request_id):
 
 
 
-#This function is for help details 
+
 
 def help_details(request : HttpRequest,request_id):
+    '''This function is for help details '''
     helprequest=HelpRequest.objects.get(id=request_id)
     reply=Reply.objects.filter(helprequest=helprequest)
     return render(request,'main/help_details.html',{'helprequest':helprequest,'reply':reply}) 
 
 
 
-#This function is for the helper profile 
+
 
 def helper_profile(request : HttpRequest, user_id):
+   '''This function is for the helper profile '''
    user = User.objects.get(id=user_id)
    return render( request, "main/helper_profile.html", {"user" : user})
 
 
 
-#This function is for the client profile 
+
 
 def client_profile(request : HttpRequest):
+   '''This function is for the client profile '''
    client=HelpRequest.objects.filter(user=request.user.id)
    return render( request, "main/client_profile.html",{'client':client})
 
